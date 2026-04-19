78da2f24835fc6cdd253d74098131ab420fbf102
asna sharin <asnasherin1233@gmail.com>
2026-04-19 20:09:14 +0530

    implement auth middleware

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
